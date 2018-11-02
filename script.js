f203c43aea17f437d5d2cc2d5b97585f45d7526f
Artem Badrtdinov <bodrych@gmail.com>
2018-11-02 16:12:12 +0500

    delete wk

--- script.js
+++ script.js
@@ -7,28 +7,28 @@ var app = new Vue({
 		// node: 'https://nodes.wavesplatform.com',
 		node: 'https://testnode1.wavesnodes.com',
 		publicKey: 'HeCW9krSqPM6AURLcgyj3PhHX3s742gPo8STVNQRM71P',
-		fee: '0.005',
+		fee: 500000,
 		last: 0,
 		maxHeight: 100,
 		maxWidth: 100,
 		cursor: {x: 0, y: 0},
 		colors: [
-			'#000000',
-			'#7e7e7e',
-			'#bebebe',
-			'#ffffff',
-			'#7e0000',
-			'#fe0000',
-			'#047e00',
-			'#06ff04',
-			'#ffff04',
-			'#7e7e00',
-			'#00007e',
-			'#0000ff',
-			'#7e007e',
-			'#fe00ff',
-			'#047e7e',
-			'#06ffff'
+		'#000000',
+		'#7e7e7e',
+		'#bebebe',
+		'#ffffff',
+		'#7e0000',
+		'#fe0000',
+		'#047e00',
+		'#06ff04',
+		'#ffff04',
+		'#7e7e00',
+		'#00007e',
+		'#0000ff',
+		'#7e007e',
+		'#fe00ff',
+		'#047e7e',
+		'#06ffff'
 		],
 		color: 2
 	},
@@ -41,9 +41,8 @@ var app = new Vue({
 			this.color = color;
 		},
 		loadTxs: async function(limit) {
-			let rawData = await fetch(this.node + '/transactions/address/' + this.canvas + '/limit/' + limit);
-			let data = await rawData.json();
-			return data;
+			let response = await axios.get(this.node + '/transactions/address/' + this.canvas + '/limit/' + limit);
+			return response.data;
 		},
 		update: async function() {
 			let lastTx = await this.loadTxs(1);
@@ -53,9 +52,6 @@ var app = new Vue({
 				this.pixels = data.slice(0);
 				this.last = lastTime.valueOf();
 			}
-		},
-		checkKeeper: function() {
-			return typeof window.Waves !== 'undefined';
 		},
 		offset: function(x, y) {
 			let offset = (x - 1) + (y - 1) * this.maxWidth;
@@ -67,8 +63,8 @@ var app = new Vue({
 			return {'x': x, 'y': y}
 		},
 		getData: async function() {
-			let rawData = await fetch(this.node + '/addresses/data/' + this.canvas);
-			let data = await rawData.json();
+			let response = await axios.get(this.node + '/addresses/data/' + this.canvas);
+			let data = response.data;
 			let filtered = await data.filter(item => {
 				let type = item.type;
 				let value = item.value;
@@ -87,26 +83,24 @@ var app = new Vue({
 		},
 		setPixel: async function(offset, color) {
 			let params = {
+				version: 1,
 				type: 12,
-				data: {
-					data: [
-						{ type: 'integer', key: offset.toString(), value: color }
-					],
-					fee: {
-						assetId: 'WAVES',
-						tokens: this.fee
-					},
-					senderPublicKey: this.publicKey
-				}
-			}
-			if (this.checkKeeper()) {
-				try {
-					let res = await window.Waves.signAndPublishTransaction(params);
-				} catch (err) {
-					alert(err.message);
-				}
-			} else {
-				alert('Please, install Waves Keeper');
+				data: [
+					{
+						type: 'integer',
+						key: offset.toString(),
+						value: color
+					}
+				],
+				fee: this.fee,
+				senderPublicKey: this.publicKey,
+				proofs: [],
+				timestamp: (new Date()).getTime()
+			};
+			try {
+				let response = await axios.post(this.node + '/transactions/broadcast', params);
+			} catch (err) {
+				alert(err.message);
 			}
 		}
 	}
